15df0f6498051a7f77c0f1b9464d49b94458e833
R3DL1N3 <r0y@digitalcombatsimulator.world>
2022-01-31 17:09:44 +0000

    Install packs

--- scratch.pl
+++ scratch.pl
@@ -7,3 +7,8 @@ format_packs :-
     query_packs('', Packs),
     forall(member(pack(Pack, _, _, _, _), Packs),
            format('~q~n', [Pack])).
+
+install_packs :-
+    query_packs('', Packs),
+    forall(member(pack(Pack, _, _, _, _), Packs),
+           pack_install(Pack, [interactive(false)])).
